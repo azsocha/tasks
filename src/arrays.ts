9cedc2c69d9ab7629e53eb7cb8161c3c807a5308
azsocha <alannazs11@gmail.com>
2026-09-09 11:50:16 -0400

    Completed tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const triple = numbers.map((numbers: number): number => numbers * 3);
+    return triple;
 }
 
 /**
