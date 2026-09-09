697e63daf09acca1517d0e1055cb4e610be26371
azsocha <alannazs11@gmail.com>
2026-09-09 12:04:18 -0400

    Completed stringsToIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -22,7 +22,11 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const Ints = numbers.map((numbers: string): number =>
+        Number.isNaN(parseInt(numbers)) ? 0 : parseInt(numbers),
+    );
+
+    return Ints;
 }
 
 /**
