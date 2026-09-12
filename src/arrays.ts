95cc76518a2d3ad0e7fb00282075ef9f365ac396
azsocha <alannazs11@gmail.com>
2026-09-12 17:38:45 -0400

    Completed makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -99,7 +99,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const equation = addends.join("+");
+    const sum = addends.reduce(
+        (currentTotal: number, num: number) => currentTotal + num,
+        0,
+    );
+    return sum + "=" + equation;
 }
 
 /**
